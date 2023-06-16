065b4a45fb2638a995cf08d51ba52c4ae1018dce
Anant Shanker <anant@MacBook-Air-3.local>
2023-06-16 16:08:24 +0800

    fixed chat history display

--- components/chatbox.tsx
+++ components/chatbox.tsx
@@ -3,31 +3,54 @@ import { Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Chatbot(props: any) {
-    const msgs:String[] = props.msgs;
-    const change_msgs = props.change_msgs;
     const para:string = props.para;
     const summ_para:string = props.summ_para;
     const [query, changeQuery] = useState('');
-    const strQuery:string = query
-    const [currMsgs, changeCurrMsgs] = useState<Object[]>(
-        [{}, {}, {}, {}, {
-        role: 'user',
-        content: "How can I help?"
-    }]);
+    let strQuery:string = query
 
-    const data = {
-        newMsg: strQuery,
-        role: "user",
-        para: para,
-        summ_para: summ_para
-    };
-
+    const [msgs, changeMsgs] = useState<Object[]>(
+        [
+            {
+                role: "user",
+                content:"Could you summarize the following paragraph for me?"
+            },
+            {
+                role: "assistant",
+                content:"Sure! Send the paragraph here."
+            },
+            {
+                role: "user",
+                content: para
+            },
+            {
+                role: "assistant",
+                content: summ_para
+            },
+            {
+                role: "assistant",
+                content: "How can I help?"
+            }
+        ]);
+    let messages:Object[] = msgs;
+    let data:Object; 
     const handleInput = (e: any) => {
         changeQuery(e.currentTarget.value)
     }
 
     const handleSend = (e: any) => {
+
         e.preventDefault();
+
+        strQuery = query;
+
+        messages = messages.concat([{
+            role: "user",
+            content: strQuery
+        }])
+        
+        data = {
+            msgs: messages
+        };
         fetch("/api/msgs", {
         method: 'POST',
         headers: {
@@ -37,17 +60,20 @@ function Chatbot(props: any) {
         })
         .then((response) => response.json())
         .then((data) => {
-        changeCurrMsgs(data.msgs);
+        messages = messages.concat([{
+            role: "assistant",
+            content: data.responseText
+        }])
+        changeMsgs(messages);
         })
         .catch((error) => {
         console.error("Error fetching data", error);
         });
-        
     }
 
     return (
         <Row className="w-[1200px] h-fit text-black bg-white rounded-lg py-4 px-20">
-            {currMsgs.filter((cont, i:number) => i > 3).map((msgObj: any, i: number) => (
+            {msgs.filter((cont, i:number) => i > 3).map((msgObj: any, i: number) => (
                 i%2 ? (
                     <div key={i} className="p-4">
                     <Col className="bg-green-600 p-2 ml-[700px] w-fit h-fit rounded-lg">
--- pages/api/msgs.ts
+++ pages/api/msgs.ts
@@ -10,60 +10,18 @@ export default async function msgs(
     res: NextApiResponse<any>
 ) {
     try {
-        let para:string = req.body.para;
-        let summ_para:string = req.body.summ_para;
-        
-        let msgs:Object[] = [
-            {
-                role: "user",
-                content:"Could you summarize the following paragraph for me?"
-            },
-            {
-                role: "assistant",
-                content:"Sure! Send the paragraph here."
-            },
-            {
-                role: "user",
-                content: para
-            },
-            {
-                role: "assistant",
-                content: summ_para
-            },
-            {
-                role: "assistant",
-                content: "How can I help?"
-            }
-        ]
 
-        const newMsg:string = req.body.newMsg;
-        const role:string = req.body.role;
-        console.log(msgs);
-        msgs.push(
-            {
-                role: role,
-                content: newMsg
-            }
-        )
-        
-        if(role == "user") {
-            const aiResponse = await openai.createChatCompletion({
-                model: "gpt-3.5-turbo",
-                messages: msgs
-            });
+        let msgs:Object[] = req.body.msgs;
 
-            const responseText = aiResponse.data.choices[0].message.content;
-            msgs = msgs.concat(
-                {
-                    role: "assistant",
-                    content: responseText
-                }
-            );
-            console.log(msgs);
-            res.status(200).json({ msgs })
-        }
+
+        const aiResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: msgs
+        });
+
+        const responseText = aiResponse.data.choices[0].message.content;
+        res.status(200).json({ responseText })
     } catch (e) {
         console.error(e);
     }
-
 }
