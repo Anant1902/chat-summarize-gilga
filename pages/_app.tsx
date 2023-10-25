4aa959dd6de25c5dcbbc760845476cbe8c195a2d
Anant Shanker <anant@MacBook-Air-51.local>
2023-10-26 01:18:02 +0800

    fix: cleanups

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import NavBar from '@/components/Navbar' 
-import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      {/* <NavBar /> */}
       <div className='py-10 px-12 w-screen h-screen'>
       <Component className='pt-10' {...pageProps} />
       </div>
